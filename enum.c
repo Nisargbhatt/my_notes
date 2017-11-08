d7fb1d84b682d9d388a6d79b537d8415ab07f57c
Nisargbhatt <bhattnisarg179@gmail.com>
2017-11-08 02:16:10 -0500

    Example for enum

---
+++ enum.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+//the enum is defined for the week which have following days
+
+enum week{mon,tue,wed,thur,fri,sat,sun};
+
+
+int main()
+{  
+    enum week day;
+    day = wed;
+    printf("%d",day);
+    return 0;
+
+
+}
