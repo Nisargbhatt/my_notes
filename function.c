e74ca7ad3f64f4dc744a3764dc5b5c70d99aa3a4
Nisargbhatt <bhattnisarg179@gmail.com>
2017-11-08 02:41:48 -0500

    Example of function

---
+++ function.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int sum(int a,int b)
+{
+    int sum;
+    printf("%d",sum=a+b);
+    return 0;
+
+}
+
+int main()
+{
+    printf("This function will redirect to sum\n");
+    
+    sum(10,12);
+    return 0;
+
+}
+
